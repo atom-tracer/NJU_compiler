fb6979c50dec9b2e077e7a047a653c21bf9589a7
thdlrt <1040880153@qq.com>
2024-04-15 00:22:04 +0800

    <template> <div class='filter-box'> <div class='left-box'> <el-input placeholder='请输入授权名称' v-model='filter.name' class='common-input' @input='query' clearable suffix-icon='el-icon-search' /> </div> <div class='right-box'>  <el-button type='primary'>新建授权申请</el-button> </div>  </div> </template>
    <script lang='ts'>
    import { defineComponent, reactive } from 'vue'
    
    export default defineComponent({
    name: 'filter-box',
    emits: ['query'],
    setup(props, _) {
    const filter = reactive({
      name: ''
    })
      const query = () => {
     _.emit('query', filter)
    }
      return {
      filter,
      query
     }
    }
    })
    </script>
    
    <style lang='stylus' scoped>
    .filter-box
      display flex
      height 32px
      justify-content space-between
      margin-bottom 16px
    </style>

--- Code/type.h
+++ Code/type.h
@@ -14,74 +14,9 @@ struct Type_{
         struct {char* name; Type type; Type next;} tail;//函数或结构体；注意：函数的返回值作为第一个类型参数
     } content;
 };
-// 创建类型
-Type createBasic(int basic)
-{
-    Type type = (Type)malloc(sizeof(struct Type_));
-    type->kind = BASIC;
-    type->content.basic = basic;
-    return type;
-}
-Type createArray(Type elem, int size)
-{
-    Type type = (Type)malloc(sizeof(struct Type_));
-    type->kind = ARRAY;
-    type->content.array.elem = elem;
-    type->content.array.size = size;
-    return type;
-}
-Type createStructure(char* name, int num, ...){
-    Type type = (Type)malloc(sizeof(struct Type_));
-    type->kind = STRUCTURE;
-    type->content.tail.name = name;
-    va_list args;
-    va_start(args, num);
-    Type p = type;
-    for(int i=0;i<num;i++){
-        p->content.tail.type = va_arg(args, Type);
-        p->content.tail.next = (Type)malloc(sizeof(struct Type_));
-        p = p->content.tail.next;
-        p->content.tail.next = NULL;
-    }
-    return type;
-}
-Type createFunction(Type ret, int num, ...){
-    Type type = (Type)malloc(sizeof(struct Type_));
-    type->kind = FUNCTION;
-    type->content.tail.type = ret;
-    va_list args;
-    va_start(args, num);
-    type->content.tail.next = (Type)malloc(sizeof(struct Type_));
-    Type p = type->content.tail.next;
-    p->content.tail.next = NULL;
-    for(int i=0;i<num;i++){
-        p->content.tail.type = va_arg(args, Type);
-        p->content.tail.next = (Type)malloc(sizeof(struct Type_));
-        p = p->content.tail.next;
-        p->content.tail.next = NULL;
-    }
-    return type;
-}
-//比较两个类型是否相同
-int compareType(Type a, Type b){
-    if(a->kind != b->kind) return 0;
-    switch(a->kind){
-        case BASIC:
-            return a->content.basic == b->content.basic;
-        case ARRAY:
-            return compareType(a->content.array.elem, b->content.array.elem);
-        case STRUCTURE:
-            return strcmp(a->content.tail.name, b->content.tail.name) == 0&&a->content.tail.name!=NULL;
-        case FUNCTION:
-            Type p1 = a;
-            Type p2 = b;
-            while(p1 && p2){
-                if(!compareType(p1->content.tail.type, p2->content.tail.type)) return 0;
-                p1 = p1->content.tail.next;
-                p2 = p2->content.tail.next;
-            }
-            return p1 == p2;
-        default:
-            assert(0);
-    }
-}
+Type createBasic(int basic);
+Type createArray(Type elem, int size);
+Type createStructure(char* name, int num, ...);
+Type createFunction(Type ret, int num, ...);
+int compareType(Type a, Type b);
+
