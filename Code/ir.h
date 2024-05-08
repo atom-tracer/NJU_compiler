e98f8b0b82d72aa0799f1167c5c2d3f37d2ab7e1
DragonK <1429281267@qq.com>
2024-05-08 15:05:59 +0800

    ir.h

--- Code/ir.h
+++ Code/ir.h
@@ -3,29 +3,31 @@
 #include "tools.h"
 #include "type.h"
 #include <assert.h>
-bool Program(TreeNode *root);
+bool compareName(TreeNode *root, int num, ...);
+bool check_func_definition();
+char *translate_Program(TreeNode *root);
 char *translate_ExtDefList(TreeNode *root);
-char *translate_ExtDef(TreeNode *root);
-bool ExtDecList(TreeNode *root, Type type);
 Type Specifier(TreeNode *root);
+char *translate_ExtDef(TreeNode *root);
+char *translate_ExtDecList(TreeNode *root, Type type);
 Type StructSpecifier(TreeNode *root);
 char *OptTag(TreeNode *root);
 char *Tag(TreeNode *root);
-Type VarDec(TreeNode *root, Type type, Type stru);
-bool FunDec(TreeNode *root, enum FunctionType functiontype, Type ret);
-bool VarList(TreeNode *root, Type type, StructureField *field);
-bool ParamDec(TreeNode *root, Type func, StructureField *field);
-char *CompSt(TreeNode *root);
+char *translate_VarDec(TreeNode *root, Type type, bool isVarlist);
+char *VarDec_id(TreeNode *root);
+char *translate_FunDec(TreeNode *root);
+char *translate_VarList(TreeNode *root);
+char *translate_ParamDec(TreeNode *root);
+char *translate_CompSt(TreeNode *root);
 char *translate_StmtList(TreeNode *root);
-bool Stmt(TreeNode *root, Type type);
-char *translate_DefList(TreeNode *root);
-bool Def(TreeNode *root, Type type);
-bool DecList(TreeNode *root, Type type, Type stru);
-bool Dec(TreeNode *root, Type type, Type stru);
-bool Args(TreeNode *root, StructureField *field); // field为返回的参数列表
+char *translate_Stmt(TreeNode *root);
+char *translate_Cond(TreeNode *root, char *label_true, char *label_false);
+char *translate_DefList(TreeNode *root, bool isstru);
+char *translate_Def(TreeNode *root);
+char *translate_DecList(TreeNode *root, Type type);
+char *translate_Dec(TreeNode *root, Type type);
+char *translate_Exp(TreeNode *root, Variable *place);
+char *translate_Args(TreeNode *root, Variable **field, int *cnt);
 char *new_label();
 char *new_temp();
-char* translate_Exp(TreeNode *root, char*place);
-char *translate_Cond(TreeNode *root, char *label_true, char *label_false);
-char *translate_Args(TreeNode *root, StructureField *field);
 char *get_relop(TreeNode *root);
